c281999d8a89a02b6fb12e8f8a73d71a31b65df1
isefrog38 <isefroge38@mail.ru>
2021-12-18 01:56:06 +0300

    18.12

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-function greet() {
-    return "hello world!";
-}
+// function greet() {
+//     return "hello world!";
+// }
 
-function rowSumOddNumbers(n) {
-    return Math.pow(n, 3);
-}
+// function rowSumOddNumbers(n) {
+//     return Math.pow(n, 3);
+// }
 
-function positiveSum(arr) {
-    var total = 0;
-    for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
-        if (arr[i] > 0) {                   // if arr[i] is greater than zero
-            total += arr[i];                  // add arr[i] to total
-        }
-    }
-    return total;
-}
+// function positiveSum(arr) {
+//     var total = 0;
+//     for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+//         if (arr[i] > 0) {                   // if arr[i] is greater than zero
+//             total += arr[i];                  // add arr[i] to total
+//         }
+//     }
+//     return total;
+// }
 
+function squareSum(numbers)
+{ 
+  var totalSum = 0;
+  for (i=0;i<numbers.length;i++)
+  {
+    totalSum += Math.pow(numbers[i], 2);
+  }
+  return totalSum;
+}
